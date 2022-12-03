ee8d167187931c20b836664bc6222786e7483e78
richie <richie.kwon1@gmail.com>
2022-12-03 17:17:35 +0000

    initial

---
+++ server.js
@@ -0,0 +1,9 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql``
+
+const server = new ApolloServer({ typeDefs })
+
+server.listen().then(
+    ({ url }) => { console.log(`Running on ${url}`) }
+);
